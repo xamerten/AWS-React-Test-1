b07d72dea95651c0c1a52da6c041771d87635030
xamerten <xamertenpub@gmail.com>
2023-03-04 17:01:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "https://cdn.skypack.dev/react@17.0.1";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 import App from './App';
 
 ReactDOM.render(
