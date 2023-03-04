fe9032aa121301f9b7ce418b98e25d77e374d423
xamerten <xamertenpub@gmail.com>
2023-03-04 16:03:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <MyComponent />
     </div>
   );
 }
